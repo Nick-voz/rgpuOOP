e3f79a5f81e190763005a0b19672a785d8d2c667
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-03 19:34:35 +0300

    implement ImageOnCanvas and include it into Bus

--- laba_3/bus.py
+++ laba_3/bus.py
@@ -1,11 +1,15 @@
-import os
+from enum import Enum
+from enum import auto
+from os import getenv
 from tkinter import Canvas
 from tkinter import Tk
 from tkinter.constants import BOTTOM
 from tkinter.constants import LEFT
+from tkinter.constants import NW
 from tkinter.constants import TOP
 from tkinter.ttk import Frame
 from tkinter.ttk import LabelFrame
+from typing import Tuple
 
 import dotenv
 from PIL import Image
@@ -14,6 +18,56 @@ from PIL import ImageTk
 from tkinter_extended.focus_sensitive_elems import Button
 from tkinter_extended.labeled_entry import LabeledEntryField
 from tkinter_extended.utils import Point
+
+
+class ImageOnCanvas:
+    def __init__(
+        self,
+        canvas: Canvas,
+        image_path: str,
+        root: Point,
+        size: Tuple[int, int] = (70, 50),
+    ):
+        self._canvas = canvas
+        self._size = size
+        self._image = self.__load_image(image_path)
+        self._root = root
+
+    def draw(self):
+        self._img_id = self._canvas.create_image(
+            self._root.x, self._root.y, image=self._image, anchor=NW
+        )
+
+    def move_on(self, delta_x: int, delta_y: int):
+        delta_x, delta_y = self.__validate_cords(delta_x, delta_y)
+        self._root.x += delta_x
+        self._root.y += delta_y
+
+    def move_to(self, point: Point):
+        self._root = point
+
+    def clear(self):
+        self._canvas.delete(self._img_id)
+
+    @staticmethod
+    def __validate_cords(x, y) -> Tuple[int, int]:
+        try:
+            _x = int(x)
+            _y = int(y)
+        except ValueError as e:
+            raise e
+        return (_x, _y)
+
+    def __load_image(self, path: str) -> ImageTk.PhotoImage:
+        image = Image.open(path)
+        image = image.resize((self._size[0], self._size[1]))
+        return ImageTk.PhotoImage(image)
+
+
+class State(Enum):
+    moving = auto()
+    created = auto()
+    stoped = auto()
 
 
 class Bus:
@@ -118,4 +172,5 @@ def set_up_menu():
 
 if __name__ == "__main__":
     root = set_up_menu()
-    root.mainloop()
+    if root:
+        root.mainloop()
