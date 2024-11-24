a2afef4ca180c1cbbb23938df62d997c7450e6c1
«Nick-voz» <nik.vozisow@gmail.com>
2024-11-24 21:25:10 +0300

    add setup laba_4 for common setup

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from laba_2.set_up import set_up_laba_2
 from laba_3.set_up import set_up_laba_3
 from tkinter_extended.set_up_util import set_up
 
+from laba_4.set_up import set_up_laba_4
+
 
 def set_up_main(frame: Frame):
     frame.winfo_toplevel().title("Main")
-    buttons = (("laba 2", set_up_laba_2), ("laba 3", set_up_laba_3))
+    buttons = (
+        ("laba 2", set_up_laba_2),
+        ("laba 3", set_up_laba_3),
+        ("laba_4", set_up_laba_4),
+    )
     set_up(frame, buttons)
 
 
