6abad484bbb6ebd376c67a9d3534d12a98662ab5
«Nick-voz» <nik.vozisow@gmail.com>
2024-12-08 19:22:29 +0300

    add students to set up function in laba_4

--- laba_4/set_up.py
+++ laba_4/set_up.py
@@ -4,11 +4,13 @@ from typing import Callable
 from tkinter_extended.set_up_util import set_up
 
 from laba_4.exam import set_up_exam
+from laba_4.students import set_up_student_card
 
 
 def set_up_laba_4(frame: Frame, callback: Callable):
     frame.winfo_toplevel().title("Laba 4")
     buttons = [
         ("exam", set_up_exam),
+        ("student card", set_up_student_card),
     ]
     set_up(frame, buttons, callback)
